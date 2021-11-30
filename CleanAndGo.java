61cd274b6ed9a17e521893c2cf031b3103408d60
CLRminded <the_rocket_scientist@icloud.com>
2021-11-30 15:03:00 -0800

    scenerio2

--- CleanAndGo.java
+++ CleanAndGo.java
@@ -2,9 +2,48 @@
 class CleanAndGo {
     public static void main(String[] args) {
         printTitle();
-
+        scenerio2();
     }
 
+    /*
+     ************************************************************
+     * Welcome To Clean-And-Go Shop
+     * 2. Customers & Services
+     ************************************************************
+     */
+
+    public static void scenerio2() {
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 15; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("Welcome To Clean-And-Go Shop");
+        for (int i = 0; i < 18; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("2. Customers & Services");
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    /*
+     *****************************************************************************
+     ***********
+     * Welcome To Clean-And-Go Shop
+     ***********
+     *****************************************************************************
+     * 1. Equipment and Supplies
+     * 2. Customers and Services
+     * 3. Employees
+     * 4. Updates
+     * 5. Quit
+     * 
+     */
     public static void printTitle() {
         for (int i = 0; i < 77; i++) {
             System.out.print("*");
@@ -31,6 +70,7 @@ class CleanAndGo {
         for (int i = 0; i < 77; i++) {
             System.out.print("*");
         }
+        System.out.println();
     }
 
 }

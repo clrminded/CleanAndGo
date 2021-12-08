5d9e811e9e68c1165137a1a1fd246fedb7dafc92
HollowTwilight <95319457+HollowTwilight@users.noreply.github.com>
2021-12-08 01:09:34 -0800

    Update Updates.java

--- Updates.java
+++ Updates.java
@@ -70,10 +70,10 @@ public static void changeEquipment(){
             System.out.flush();
             String ch = CleanAndGo.readLine();
             System.out.println();
+            Statement stmt = conn.createStatement();
 
             switch (ch.charAt(0)){
                 case '1':
-                    Statement stmt = conn.createStatement();
                     System.out.print("Brand name: ");
                     System.out.flush();
                     String brand = CleanAndGo.readLine();
@@ -94,22 +94,73 @@ public static void changeEquipment(){
                     Date RetiredDate = CleanAndGo.readLine();
                     System.out.print("Supplier ID: ");
                     System.out.flush();
-                    INT SupplierID = CleanAndGo.readLine();
+                    Integer SupplierID = CleanAndGo.readLine();
                     System.out.print("Cycle Length in Minutes: ");
                     System.out.flush();
-                    INT CycleLengthMin = CleanAndGo.readLine();
+                    Integer CycleLengthMin = CleanAndGo.readLine();
 
 
                     String query = "INSERT INTO Equipment(Brand, PurchaseDate, PurchasePrice, Type, LifeSpanMonths, RetiredDate, SupplierID, CycleLengthMin) "
                      + "VALUES (" + brand + ", " + purchaseDate + ", " + purchasePrice + ", " + equipmentType + ", " + LifeSpanMonths + ", "
                      + RetiredDate + ", " + SupplierID + ", " + CycleLengthMin + ")";
                     stmt.executeQuery(query);
+
+                    query = "SELECT id, Brand from Equipment;";
+                    ResultSet rset = stmt.executeQuery(query);
+                    Integer equipmentID;
+                    
                     System.out.println("Addition Complete!");
+                    System.out.println("                       Equipment List");
+                    System.out.println("-------------------------------------------------------------");
+                    while(rset.next()){
+                        equipmentID = rset.getInt(1);
+                        String brandName = rset.getString(2);
+                        System.out.println("Equipment ID: "+equipmentID+ " Brand Name: "+brandName);
+                    };
                     break;
                 case '2':
+                    System.out.print("Enter Equipment ID you wish to delete: ");
+                    System.out.flush();
+                    equipmentID = CleanAndGo.readLine();
+
+                    query = "DELETE FROM Equipment WHERE id="+ equipmentID + ";";
+                    stmt.executeQuery(query); 
                     System.out.println("Deletion Complete!");
                     break;
                 case '3':
+                    System.out.print("Enter Equipment ID you wish to update: ");
+                    System.out.flush();
+                    equipmentID = CleanAndGo.readLine();
+
+                    System.out.print("Brand name: ");
+                    System.out.flush();
+                    brand = CleanAndGo.readLine();
+                    System.out.print("Purchase Date YYYY-MM-DD: ");
+                    System.out.flush();
+                    purchaseDate = CleanAndGo.readLine();
+                    System.out.print("Purchase Price: ");
+                    System.out.flush();
+                    purchasePrice = CleanAndGo.readLine();
+                    System.out.print("Equipment Type: ");
+                    System.out.flush();
+                    equipmentType = CleanAndGo.readLine();
+                    System.out.print("Equipment Lifespan (Months): ");
+                    System.out.flush();
+                    LifeSpanMonths = CleanAndGo.readLine();
+                    System.out.print("Retire Date YYYY-MM-DD: ");
+                    System.out.flush();
+                    RetiredDate = CleanAndGo.readLine();
+                    System.out.print("Supplier ID: ");
+                    System.out.flush();
+                    SupplierID = CleanAndGo.readLine();
+                    System.out.print("Cycle Length in Minutes: ");
+                    System.out.flush();
+                    CycleLengthMin = CleanAndGo.readLine();
+
+                    query = "UPDATE Equipment " + "SET Brand="+brand+", "+ "PurchaseDate="+purchaseDate+", "+ "PurchasePrice="+purchasePrice+", "
+                    +"Type="+equipmentType+"; "+ "LifeSpanMonths="+LifeSpanMonths+", "+ "RetiredDate="+RetiredDate+", "+ "SupplierID="+SupplierID+", "
+                    +"CycleLengthMin="+CycleLengthMin+", " + "WHERE id=" + equipmentID+";";
+                    stmt.executeQuery(query);
                     System.out.println("Update Complete!");
                     break;
                 case '4':
